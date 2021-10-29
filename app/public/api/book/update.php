f7b8ab3c083a29e460fb7b867f69854e2a98627b
mkhaing <moneikhaing@gmail.com>
2021-10-29 09:50:57 -0400

    Edit, Delete, Add Book

--- app/public/api/book/update.php
+++ app/public/api/book/update.php
@@ -31,19 +31,32 @@ $db = DbConnection::getConnection();
 // Step 2: Create & run the query
 // Note the use of parameterized statements to avoid injection
 $stmt = $db->prepare(
-  'DELETE from book WHERE id=?'
+  'UPDATE book SET
+    title = ?,
+    author = ?,
+    yearPublished = ?,
+    publisher = ?,
+    pageCount = ?,
+    msrp=?
+  WHERE id = ?'
 );
 
 $stmt->execute([
+  $_POST['title'],
+  $_POST['author'],
+  $_POST['yearPublished'],
+  $_POST['publisher'],
+  $_POST['pageCount'],
+  $_POST['msrp'],
   $_POST['id']
 ]);
 
 // Get auto-generated PK from DB
 // https://www.php.net/manual/en/pdo.lastinsertid.php
- $pk = $db->lastInsertId();  
+// $pk = $db->lastInsertId();  
 
 // Step 4: Output
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
 header('HTTP/1.1 303 See Other');
-header('Location: ../book/');
+header('Location: ../book/?books=' . $_POST['title']);
--- app/public/js/index.js
+++ app/public/js/index.js
@@ -2,7 +2,9 @@ const SomeApp = {
   data() {
     return {
       books: [],
-      bookForm:{}
+      bookForm:{},
+      selectedBook: null,
+
     }
   },
   computed: {},
@@ -18,8 +20,22 @@ const SomeApp = {
               console.error(err);
           })
       },
-      
-      postBook(evt) {        
+      selectBook(s) {
+        if (s == this.selectedBook) {
+            return;
+        }
+        this.selectedBook = s;
+        this.books = [];
+        this.fetchBookData(this.selectedBook);
+    },
+    postBook(evt) {
+      if (this.selectedBook === null) {
+          this.postNewBook(evt);
+      } else {
+          this.postEditBook(evt);
+      }
+    },
+      postNewBook(evt) {        
         console.log("Posting!", this.bookForm);
 
         fetch('api/book/create.php', {
@@ -38,7 +54,56 @@ const SomeApp = {
             // reset the form
             this.bookForm = {};
           });
-      }
+      },
+      postEditBook(evt) {        
+        this.bookForm.title = this.selectedBook.title;        
+        console.log("Updating!", this.bookForm);
+
+        fetch('api/book/update.php', {
+            method:'POST',
+            body: JSON.stringify(this.bookForm),
+            headers: {
+              "Content-Type": "application/json; charset=utf-8"
+            }
+          })
+          .then( response => response.json() )
+          .then( json => {
+            console.log("Returned from post:", json);
+            // TODO: test a result was returned!
+            this.books = json;
+            
+            this.resetBookForm();
+          });
+      },
+      postDeleteBook(o) {
+        if (!confirm("Are you sure you want to delete the book "+o.title+"?")) {
+            return;
+        }
+        
+        fetch('api/book/delete.php', {
+            method:'POST',
+            body: JSON.stringify(o),
+            headers: {
+              "Content-Type": "application/json; charset=utf-8"
+            }
+          })
+          .then( response => response.json() )
+          .then( json => {
+            console.log("Returned from post:", json);
+            // TODO: test a result was returned!
+            this.books = json;
+            
+            this.resetBookForm();
+          });
+      },
+      selectBookForEdit(o) {
+        this.selectedBook = o;
+        this.bookForm = Object.assign({}, this.selectedBook);
+    },
+      resetBookForm() {
+        this.selectedBook = null;
+        this.bookForm = {};
+      },
       },
   created() {
       this.fetchBookData();
